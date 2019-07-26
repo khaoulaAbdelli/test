926067f583fff4d6c1c34ae4db59b089e4cb1425
kabd <kabd@OSBlap314.idvnet.local>
2019-07-26 22:30:28 +0200

    erst_commit

---
+++ Exercices/src/interviews/Factorielle.java
@@ -0,0 +1,13 @@
+package interviews;
+
+public class Factorielle {
+	public int factorielle(int n) {
+		if (n==0) return 1 ;
+		return n*factorielle(n-1) ;
+	}
+	public static void main(String args[]) {
+		
+		
+	}
+
+}
